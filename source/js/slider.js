4314a9d0c841bb59904cc3d74c8c8844c7586fbb
tis <tisbootis@gmail.com>
2020-08-26 19:59:46 +0300

    add double swiper v.2

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,59 +1,69 @@
 // слайдер врачей
 // $(document).ready(function () {
 
-  let mainSwiper = new Swiper('#main-swiper', {
-    slidesPerView: '1',
-    // spaceBetween: 25,
-    // touchRatio: 0,
+  // let mainSwiper = new Swiper('#main-swiper', {
+  //   slidesPerView: '1',
+  //   // spaceBetween: 25,
+  //   // touchRatio: 0,
 
-    direction: 'vertical',
-    // loop: true,
+  //   direction: 'vertical',
+  //   // loop: true,
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
 
-      // dynamicBullets: true,
+  //     // dynamicBullets: true,
 
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + '<i>' + (index + 1) + '</i>' + '</span>';
-      }
-    },
+  //     renderBullet: function (index, className) {
+  //       return '<span class="' + className + '">' + '<i>' + (index + 1) + '</i>' + '</span>';
+  //     }
+  //   },
 
-    // watchSlidesVisibility: true,
+  //   // watchSlidesVisibility: true,
 
-    navigation: {
-      nextEl: '.main-swiper__button-next',
-      prevEl: '.main-swiper__button-prev',
-    },
+  //   navigation: {
+  //     nextEl: '.main-swiper__button-next',
+  //     prevEl: '.main-swiper__button-prev',
+  //   },
 
-    // breakpoints: {
-    //   756: {
-    //     slidesPerView: '3',
-    //     spaceBetween: 20,
-    //   },
-    // }
-  });
+  //   // breakpoints: {
+  //   //   756: {
+  //   //     slidesPerView: '3',
+  //   //     spaceBetween: 20,
+  //   //   },
+  //   // }
+  // });
 // });
 
-// слайдер клиник
-// $(document).ready(function () {
+let mainSwiperPagination = new Swiper('#main-swiper .pagination-swiper-container', {
+  slidesPerView: '3',
+  direction: 'vertical',
+  // loop: true,
 
-//   let rehabsSwiper = new Swiper('#rehabs-swiper', {
-//     slidesPerView: '2',
-//     spaceBetween: 25,
-//     touchRatio: 1,
+  // navigation: {
+  //   nextEl: '.pagination-swiper-button-next',
+  //   prevEl: '.pagination-swiper-button-prev',
+  // },
+});
 
-//     navigation: {
-//       nextEl: '.rehabs-swiper__button-next',
-//       prevEl: '.rehabs-swiper__button-prev',
-//     },
 
-//     breakpoints: {
-//       756: {
-//         slidesPerView: '4',
-//         spaceBetween: 20,
-//       },
-//     }
-//   });
-// });
+let mainSwiper = new Swiper('#main-swiper .content-swiper-container', {
+  slidesPerView: '1',
+  direction: 'vertical',
+  // loop: true,
+
+  // navigation: {
+  //   nextEl: '.main-swiper__button-next',
+  //   prevEl: '.main-swiper__button-prev',
+  // },
+
+  navigation: {
+    nextEl: '.pagination-swiper-button-next',
+    prevEl: '.pagination-swiper-button-prev',
+  },
+
+  thumbs: {
+    swiper: mainSwiperPagination
+  }
+});
