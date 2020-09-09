38e9f63cb48efc08579a0351d3d3eef2c106a180
tis <tisbootis@gmail.com>
2020-09-09 19:39:15 +0300

    add double swiper v.5 (include long-pagination)

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,8 +5,31 @@
 // 3) Рендерим параметры главного слайдера (c зависимостью от thumbs).
 // 4) Рендерим главный слайдер с параметрами.
 
+// Параметрами в renderSwiper() передаём:
+// 1) id слайдера;
+// 2) направление ('vertical'/'horizontal'),
+//    у контейнеров с id должны быть соответствующие классы в разметке ('.vertical'/'.horizontal');
+// 3) длинная пагинация (true/false),
+//    у контейнера с id должен быть соответствующий класс в разметке ('.long-pagination');
+
+// слайдер главный (index, первый экран)
+renderSwiper('#main-swiper', 'vertical');
+
+// слайдер проектов
+renderSwiper('#project-swiper', 'vertical');
+
+// слайдер событий
+renderSwiper('#event-swiper', 'horizontal');
+
+// слайдер детального проекта ("Салют")
+renderSwiper('#project-detail-swiper', 'horizontal');
+
+// слайдер наград (с длинной пагинацией)
+renderSwiper('#reward-swiper', 'horizontal', true);
+
 // параметры пагинации
-function renderSwiperThumbsParam(direction) {
+function renderSwiperThumbsParam(direction, longPagination) {
+
   let swiperThumbsParam = {
     slidesPerView: '3',
     direction: 'horizontal',
@@ -27,11 +50,59 @@ function renderSwiperThumbsParam(direction) {
     }
   }
 
+  // если у слайдера длинная пагинация
+  if (longPagination) {
+    swiperThumbsParam.breakpoints['768'].slidesPerView = 12;
+  }
+
+
+  // if (longPagination) {
+  //   swiperThumbsParam = {
+  //     slidesPerView: '3',
+  //     direction: 'horizontal',
+  //     watchSlidesVisibility: true,
+  //     watchSlidesProgress: true,
+
+  //     breakpoints: {
+  //       768: {
+  //         direction: direction,
+  //         slidesPerView: '12'
+  //       },
+  //       600: {
+  //         slidesPerView: '5'
+  //       },
+  //       500: {
+  //         slidesPerView: '4'
+  //       }
+  //     }
+  //   }
+  // } else if (!longPagination) {
+  //   swiperThumbsParam = {
+  //     slidesPerView: '3',
+  //     direction: 'horizontal',
+  //     watchSlidesVisibility: true,
+  //     watchSlidesProgress: true,
+
+  //     breakpoints: {
+  //       768: {
+  //         direction: direction,
+  //         slidesPerView: '3'
+  //       },
+  //       600: {
+  //         slidesPerView: '5'
+  //       },
+  //       500: {
+  //         slidesPerView: '4'
+  //       }
+  //     }
+  //   }
+  // }
+
   return swiperThumbsParam;
 }
 
 // параметры главного слайдера
-function renderSwiperContentParam(id, direction, thumbsName) {
+function renderSwiperContentParam(id, direction, thumbsName, longPagination) {
   let swiperContentParam = {
     slidesPerView: '1',
     direction: 'horizontal',
@@ -51,34 +122,33 @@ function renderSwiperContentParam(id, direction, thumbsName) {
         direction: direction
       }
     }
+  }
 
+  // активным делаем последний слайд
+  if (longPagination) {
+    makeInitialSlideOfLast(id, swiperContentParam);
   }
 
   return swiperContentParam;
 }
 
-function renderSwiper(id, direction) {
-  // опционально
-  // document.querySelector(id).classList.add(direction);
-
-  let swiperThumbsParam = renderSwiperThumbsParam(direction);
-  let swiperThumbs = new Swiper(id + ' .pagination-swiper-container', swiperThumbsParam);
-
-  let swiperContentParam = renderSwiperContentParam(id, direction, swiperThumbs);
-  let swiperContent = new Swiper(id + ' .content-swiper-container', swiperContentParam);
+// если длинный слайдер - при инициализации активным делаем последний слайд
+function makeInitialSlideOfLast(id, swiperParam) {
+ let currentSlidesLength = document.querySelectorAll(id + ' .swiper-slide.pagination-swiper-slide:not(.swiper-slide-duplicate)').length;
+ swiperParam.initialSlide = currentSlidesLength;;
 }
 
-// параметрами передаём id и направление ('vertical'/'horizontal')
-// у контейнеров с id должны быть соответствующие классы ('.vertical'/'.horizontal')
+function renderSwiper(id, direction, longPagination) {
+  // опционально
+  // let currentSwiper = document.querySelector(id);
+  // currentSwiper.classList.add(direction);
+  // if (longPagination) {
+  //   currentSwiper.classList.add('long-pagination');
+  // }
 
-// слайдер главный (index, первый экран)
-renderSwiper('#main-swiper', 'vertical');
+  let swiperThumbsParam = renderSwiperThumbsParam(direction, longPagination);
+  let swiperThumbs = new Swiper(id + ' .pagination-swiper-container', swiperThumbsParam);
 
-// слайдер проектов
-renderSwiper('#project-swiper', 'vertical');
-
-// слайдер событий
-renderSwiper('#event-swiper', 'horizontal');
-
-// слайдер детального проекта ("Салют")
-renderSwiper('#project-detail-swiper', 'horizontal');
+  let swiperContentParam = renderSwiperContentParam(id, direction, swiperThumbs, longPagination);
+  let swiperContent = new Swiper(id + ' .content-swiper-container', swiperContentParam);
+}
